89a4b5d2d6cd27e04969093c59bba48648d5da0d
pengxiangcai <pxcai@abeliancap.com>
2023-01-30 23:31:35 +0800

    forward test

--- template/main.cc
+++ template/main.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <type_traits>
+#include <utility>
 #include "type_traits.hh"
 
 void test_forward(int &&v) {
@@ -21,7 +22,7 @@ void test(T &&v) {
     // 优先推到出T&版本，而不是T&&版本
     // test_forward(type_traits::forward(v));
     // 强制指定版本
-    test_forward(type_traits::forward<T&&>(v));
+    test_forward(type_traits::forward<T>(v));
     // test_forward(type_traits::forward<int&&>(20));
 }
 
@@ -40,10 +41,39 @@ void test_t(T &&a) {
     std::cout << std::is_same_v<T, int&&> << std::endl;
 }
 
+#include <utility>
+#include <iostream>
+
+void overloaded(const int& x)
+{
+  std::cout << "[lvalue]" << std::endl;
+}
+
+void overloaded(int&& x)
+{
+  std::cout << "[rvalue]" << std::endl;
+}
+
+template <class T> void fn(T&& x)
+{
+  overloaded(x);
+  overloaded(std::forward<T>(x));
+}
+
+void test_forward1()
+{
+  int i = 10;
+  overloaded(std::forward<int>(i));
+  overloaded(std::forward<int&>(i));
+  overloaded(std::forward<int&&>(i));
+
+  fn(i);
+  fn(std::move(i));
+}
+
 int main() {
     test_forward();
-    int i = 2;
-    // test_t<int&>(i);
-    // test_t<int&&>(0);
+    test_forward1();
+
     return 0;
 }
