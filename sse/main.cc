42b8472d47046ba87ffbf6e40511a697f6b25405
pengxiangcai <pxcai@abeliancap.com>
2023-03-16 17:14:32 +0800

    test mm_cmpeq_epi8

---
+++ sse/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <unordered_map>
+
+#include "mm_cmpeq_epi8.hh"
+
+int main() {
+    test_mm_cmpeq_epi8();
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
---
+++ sse/mm_cmpeq_epi8.hh
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <iostream>
+
+#include <emmintrin.h>
+#include <smmintrin.h>
+
+// 参考：https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html#techs=SSE_ALL&ig_expand=963,937,945,955
+
+inline void test_mm_cmpeq_epi8() {
+    char arr1[] = "0123456789abcdef";   //每个字符占1B，一共16B=128bit
+    char arr2[] = "0123456789abcded";
+                 //  1111111111111000
+                 //  按照相同为1，不同为0来比较。
+
+    __m128i arr1_16B = _mm_load_si128((const __m128i *)arr1);
+    __m128i arr2_16B = _mm_load_si128((const __m128i *)arr2);
+    __m128i cmp_res8 = _mm_cmpeq_epi8(arr1_16B, arr2_16B);
+    int mask_8 = _mm_movemask_epi8(cmp_res8);
+    __m128i cmp_res16 = _mm_cmpeq_epi16(arr1_16B, arr2_16B);
+    int mask_16 = _mm_movemask_epi8(cmp_res16);
+    __m128i cmp_res32 = _mm_cmpeq_epi32(arr1_16B, arr2_16B);
+    int mask_32 = _mm_movemask_epi8(cmp_res32);
+
+    // CPUID Flags: SSE4.1
+    // __m128i cmp_res64 = _mm_cmpeq_epi64(arr1_16B, arr2_16B);
+    // int mask_64 = _mm_movemask_epi8(cmp_res64);
+
+    std::cout << "mask_8 = " << std::hex << mask_8 << std::endl;
+    std::cout << "mask_16 = " << std::hex << mask_16 << std::endl;
+    std::cout << "mask_32 = " << std::hex << mask_32 << std::endl;
+    // std::cout << "mask_64 = " << std::hex << mask_64 << std::endl;
+
+
+}
