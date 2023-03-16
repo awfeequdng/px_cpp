6f2289df9d6ad54cc0a0f2cb9f212d88ce2e0512
pengxiangcai <pxcai@abeliancap.com>
2023-03-16 11:42:21 +0800

    test restrict

---
+++ restrict/main.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <unordered_map>
+#include <chrono>
+
+// 对restrict的分析在文章：https://zhuanlan.zhihu.com/p/614432916
+int add1(int* a, int* b)
+{
+    *a = 10;
+    *b = 12;
+    return *a + *b;
+}
+
+int add2(int* __restrict a, int* __restrict b) {
+    *a = 10;
+    *b = 12;
+    return *a + *b;
+}
+
+constexpr size_t ITER_COUNT = 100000000;
+
+int main() {
+    int *a = new int;
+    int *b = new int;
+
+    {
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        for (size_t i = 0; i < ITER_COUNT; i++) {
+            add1(a, b);
+        }
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        std::cout << "Time difference of add1 = " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() << "[ns]" << std::endl;
+    }
+
+    {
+        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+        for (size_t i = 0; i < ITER_COUNT; i++) {
+            add2(a, b);
+        }
+        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+        std::cout << "Time difference of add2 = " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() << "[ns]" << std::endl;
+    }
+
+    return 0;
+}
