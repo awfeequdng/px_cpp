faaf1d1414ebb33fc3aaaf0ebb674a94f03d0733
pengxiangcai <pxcai@abeliancap.com>
2023-04-11 15:43:49 +0800

    test long double

---
+++ test_long_double/main.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <unordered_map>
+
+
+int main() {
+    long double a;
+    double b;
+
+    std::cout << "sizeof(long double) = " << sizeof(a) << std::endl;
+    std::cout << "sizeof(double) = " << sizeof(b) << std::endl;
+
+    return 0;
+}
